d2df0a55afc19337375294970f56e07116a2ac9a
Ido Ben-Hur <idoybh2@gmail.com>
2023-05-13 19:27:38 +0300

    Create LICENSE

--- app/src/main/java/com/idoybh/yasr/MainActivity.java
+++ app/src/main/java/com/idoybh/yasr/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 Ido Ben-Hur
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.idoybh.yasr;
 
 import android.Manifest;
@@ -83,6 +98,12 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_about) {
+            (new MaterialAlertDialogBuilder(MainActivity.this)
+                    .setMessage(R.string.about)
+                    .setPositiveButton(R.string.button_ok, null)
+            ).show();
             return true;
         }
 
