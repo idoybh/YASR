147131c1c394502f34d9137e4345892d6e257756
Ido Ben-Hur <idoybh2@gmail.com>
2023-07-16 11:28:28 +0300

    Add an option to opt-out of firebase analytics

--- app/src/main/java/com/idoybh/yasr/SettingsFragment.java
+++ app/src/main/java/com/idoybh/yasr/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.app.UiModeManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.navigation.fragment.NavHostFragment;
@@ -11,6 +12,11 @@ import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +26,8 @@ public class SettingsFragment extends PreferenceFragmentCompat
     private static final String THEME_KEY = "ui_mode";
     public static final String SORT_KEY = "sort_mode";
     private static final String LANG_KEY = "lang_pref";
+    private static final String ANALYTICS_KEY = "analytics";
+    private static final String ANALYTICS_RESET_KEY = "analytics_reset";
 
     private static final Map<Integer, Integer> THEME_VALUE_TO_UI_MODE = new HashMap<>(Map.of(
             0, UiModeManager.MODE_NIGHT_NO,
@@ -28,10 +36,13 @@ public class SettingsFragment extends PreferenceFragmentCompat
     ));
 
     private UiModeManager mUiManager;
+    private FirebaseAnalytics mAnalytics;
 
     private ListPreference mThemeListPref;
     private ListPreference mSortListPref;
     private Preference mLangPref;
+    private SwitchPreference mAnalyticsPref;
+    private Preference mAnalyticsResetPref;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -40,6 +51,7 @@ public class SettingsFragment extends PreferenceFragmentCompat
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(requireActivity());
         mUiManager = (UiModeManager) requireContext()
                 .getSystemService(Context.UI_MODE_SERVICE);
+        mAnalytics = FirebaseAnalytics.getInstance(requireContext());
 
         mThemeListPref = findPreference(THEME_KEY);
         final int prefValue = Integer.parseInt(mThemeListPref.getValue());
@@ -53,6 +65,13 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
         mLangPref = findPreference(LANG_KEY);
         mLangPref.setOnPreferenceClickListener(this);
+
+        mAnalyticsPref = findPreference(ANALYTICS_KEY);
+        mAnalyticsPref.setOnPreferenceChangeListener(this);
+
+        mAnalyticsResetPref = findPreference(ANALYTICS_RESET_KEY);
+        mAnalyticsResetPref.setOnPreferenceClickListener(this);
+        updateAnalyticsSummary();
     }
 
     @Override
@@ -60,6 +79,12 @@ public class SettingsFragment extends PreferenceFragmentCompat
         if (preference == mLangPref) {
             NavHostFragment.findNavController(SettingsFragment.this)
                     .navigate(R.id.action_settingsFragment_to_langFragment);
+            return true;
+        } else if (preference == mAnalyticsResetPref) {
+            mAnalytics.resetAnalyticsData();
+            updateAnalyticsSummary();
+            Toast.makeText(requireContext(), getString(R.string.analytics_reset_done),
+                    Toast.LENGTH_LONG).show();
             return true;
         }
         return false;
@@ -76,7 +101,21 @@ public class SettingsFragment extends PreferenceFragmentCompat
             final int value = Integer.parseInt((String) newValue);
             mSortListPref.setSummary(mSortListPref.getEntries()[value]);
             return true;
+        } else if (preference == mAnalyticsPref) {
+            final boolean checked = (Boolean) newValue;
+            mAnalytics.setAnalyticsCollectionEnabled(checked);
+            updateAnalyticsSummary();
+            return true;
         }
         return false;
+    }
+
+    private void updateAnalyticsSummary() {
+        final String summary = getString(R.string.analytics_reset_summary);
+        final Task<String> task = mAnalytics.getAppInstanceId();
+        task.addOnCompleteListener(task1 -> {
+            final String id = task1.getResult();
+            mAnalyticsResetPref.setSummary(String.format(summary, id != null ? id : ""));
+        });
     }
 }
