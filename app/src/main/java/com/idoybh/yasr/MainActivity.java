f39756b830edb78cd2126240661578c9346c680f
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-09 22:09:41 +0300

    Use the material3 appbar

--- app/src/main/java/com/idoybh/yasr/MainActivity.java
+++ app/src/main/java/com/idoybh/yasr/MainActivity.java
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(findViewById(R.id.toolbar));
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavGraph navGraph = navController.getNavInflater().inflate(R.navigation.nav_graph);
 
